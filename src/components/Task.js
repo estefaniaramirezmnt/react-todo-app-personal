674fecadbc336873c2a3f553f3e0d6335d1699cd
Estefanía <estefaniaramirezmnt@gmail.com>
2024-03-08 20:55:10 +0100

    Added removeTask function

--- src/components/Task.js
+++ src/components/Task.js
@@ -4,7 +4,7 @@ import Checkbox from "./Checkbox";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrash } from "@fortawesome/free-solid-svg-icons";
 
-function Task({ name, done, onToggle }) {
+function Task({ name, done, onToggle, onTrash }) {
   return (
     <div className={"task " + (done ? "done" : "")}>
       <Checkbox checked={done} onClick={() => onToggle(!done)} />
@@ -12,7 +12,7 @@ function Task({ name, done, onToggle }) {
         <span className="task-name">{name}</span>
       </div>
       <div className="icons-container">
-        <button className="trash">
+        <button className="trash" onClick={onTrash}>
           <FontAwesomeIcon icon={faTrash} className="trash-icon" />
         </button>
       </div>
